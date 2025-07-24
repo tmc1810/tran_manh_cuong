4e885ba25e3c57d89b95ac9c7fd7be8a4ec45334
Trần Mạnh Cường <tranmanhcuong.work@gmail.com>
2025-07-24 15:43:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
     return (
         <Router>
             <nav className="navbar">
-                <Link to="/tran_manh_cuong/" className="logo">.Hồ sơ.</Link>
+                <Link to="/tran_manh_cuong" className="logo">.Hồ sơ.</Link>
                 <ul>
-                    <li className="active"><Link to="/tran_manh_cuong/">Trang chủ</Link></li>
+                    <li className="active"><Link to="/tran_manh_cuong">Trang chủ</Link></li>
                     <li><Link to="/tran_manh_cuong/about">Giới thiệu</Link></li>
                     <li><Link to="/tran_manh_cuong/projects">Dự án</Link></li>
                     <li><Link to="/tran_manh_cuong/contact">Liên hệ</Link></li>
@@ -23,7 +23,7 @@ function App() {
             </div>
 
             <Routes>
-                <Route path="/tran_manh_cuong/" element={<Home />} />
+                <Route path="/tran_manh_cuong" element={<Home />} />
                 <Route path="/tran_manh_cuong/about" element={<About />} />
                 <Route path="/tran_manh_cuong/projects" element={<Projects />} />
                 <Route path="/tran_manh_cuong/contact" element={<Contact />} />
